d10b876c52c685a53f0c49de74329f97207f9462
lhc <marco.li@ximalaya.com>
2021-07-07 23:23:27 +0800

    [feat]clean时清楚json文件、构建生成markdown文档

--- router-gradle-plugin/src/main/groovy/com/marco/router/gradle/RouterPlugin.groovy
+++ router-gradle-plugin/src/main/groovy/com/marco/router/gradle/RouterPlugin.groovy
@@ -2,6 +2,7 @@ package com.marco.router.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import groovy.json.JsonSlurper
 
 class RouterPlugin implements Plugin<Project> {
 
@@ -20,18 +21,72 @@ class RouterPlugin implements Plugin<Project> {
         }
 
         //实现旧的构建产物的自动清理
+        project.clean.doFirst {
+            File routerMapping = new File(project.rootProject.projectDir, "router_mapping")
+            if (routerMapping.exists()) {
+                routerMapping.deleteDir()
+            }
+        }
         //在javac任务后汇总生成文档
 
         println("[RouterPlugin]apply from ${project.name}")
-        //step1 定义Extension
-        //step2 注册Extension
-        //step3 使用Extension
-        //step4 获取Extension
+        //注册Extension
         project.getExtensions().create("router", RouterExtension)
         project.afterEvaluate {
+            //找到java任务
             //当前工程配置阶段结束，此处可以拿到用户配置的Extension参数
             RouterExtension extension = project["router"]
-            println("用户设置的wiki路径:${extension.wikiDir}")
+            println("[RouterPlugin]用户设置的wiki路径:${extension.wikiDir}")
+            project.tasks.findAll { task ->
+                task.name.startsWith('compile') && task.name.endsWith('JavaWithJavac')
+            }.each { task ->
+                println("[RouterPlugin]findTask:$task.name")
+                task.doLast {
+                    createMarkDown(project, extension.wikiDir)
+                }
+            }
         }
+    }
+
+    private static void createMarkDown(Project project, String dir) {
+        println("[RouterPlugin]createMarkDown")
+        File routerMapping = new File(project.rootProject.projectDir,
+                "router_mapping")
+        if (!routerMapping.exists()) {
+            return
+        }
+        File[] allChildFiles = routerMapping.listFiles()
+        if (allChildFiles.size() < 1) {
+            return
+        }
+        StringBuilder markdownBuilder = new StringBuilder()
+        markdownBuilder.append("# 页面文档\n\n")
+        markdownBuilder.append("|DESCRIPTION|URL|REAL PATH|\n")
+        markdownBuilder.append("|-|-|-|\n")
+        allChildFiles.each { child ->
+            if (child.name.endsWith('.json')) {
+                JsonSlurper jsonSlurper = new JsonSlurper()
+                def content = jsonSlurper.parse(child)
+                content.each { innerContent ->
+                    def url = innerContent['url']
+                    def des = innerContent['des']
+                    def realPath = innerContent['realPath']
+                    markdownBuilder.append("|").append(des)
+                            .append("|").append(url)
+                            .append("|").append(realPath)
+                            .append("|").append("\n")
+                }
+            }
+        }
+
+        File wikiFileDir = new File(dir)
+        if (!wikiFileDir.exists()) {
+            wikiFileDir.mkdir()
+        }
+        File wikiFile = new File(wikiFileDir, "页面文档.md")
+        if (wikiFile.exists()) {
+            wikiFile.delete()
+        }
+        wikiFile.write(markdownBuilder.toString())
     }
 }
